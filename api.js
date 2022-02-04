7963efa6ea5b1a3b56cf95ffd91fbbb0de1da8d7
swastik mund <mundswastik@gmail.com>
2022-02-05 00:30:18 +0530

    add env file

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 //jshint esversion: 8
+require('dotenv').config();
 async function allCoins(){
   var response = await fetch("https://coingecko.p.rapidapi.com/coins/markets?vs_currency=usd&page=1&per_page=100&order=market_cap_desc", {
   	"method": "GET",
   	"headers": {
   		"x-rapidapi-host": "coingecko.p.rapidapi.com",
-  		"x-rapidapi-key": "53c8c1db14msh4bce4dd874042d8p159ccbjsn81b87eabb0c4"
+  		"x-rapidapi-key": process.env.KEY
   	}
   });
   var result = response.json();
